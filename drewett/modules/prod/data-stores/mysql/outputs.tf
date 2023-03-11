50726a98550f56420f50d94c0755fe636ce66366
njdrewett <njdrewett@sky.com>
2023-03-11 18:58:14 +0000

    Modules - moving to live folder

--- drewett/modules/prod/data-stores/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value       = aws_db_instance.mysql.address
-  description = "Connection endpoint to the database"
-}
-
-output "port" {
-  value       = aws_db_instance.mysql.port
-  description = "The exposed Prt of the database connection"
-}
